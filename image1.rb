e887ebc6e3e33da262f1d8a377c05f52996d4573
leroy0615 <leroy0615@gmail.com>
2019-07-03 17:07:38 +0000

    Initialize commit

---
+++ image1.rb
@@ -0,0 +1,21 @@
+class Image1
+	def initialize(image)
+		@image= image
+	end
+
+
+	def output
+		@image.each do|m|
+			puts m.join(" ")
+		end
+	end
+end
+
+image = Image1.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
+
+image.output
